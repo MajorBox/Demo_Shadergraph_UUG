26731fd252d4c341269f26aace52f32d15b5dc16
Rod <rodrigo.ramirez.sc@gmail.com>
2019-06-21 02:01:03 -0600

    Unity Project

---
+++ Intro_Shadergraph_UUG/Assets/UUG_Intro_Shadergraph/Scripts/UpdateRefProbe.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateRefProbe : MonoBehaviour
+{
+    public ReflectionProbe rf;
+    public float renderTime = 2.0f;
+    RenderTexture targetTexture;
+    private int RenderId = -1;
+
+    IEnumerator Start()
+    {
+        rf = GetComponent<ReflectionProbe>();
+
+        rf.timeSlicingMode = UnityEngine.Rendering.ReflectionProbeTimeSlicingMode.AllFacesAtOnce;
+        while (true)
+        {
+            yield return new WaitForSeconds(renderTime);
+
+            RenderId = rf.RenderProbe(targetTexture);
+        }
+    }
+
+    private void Update()
+    {
+        if(rf.IsFinishedRendering(RenderId))
+        {
+            rf.RenderProbe(targetTexture = null);
+        }
+    }
+
+}
